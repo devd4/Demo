6e43cebbc6fcbc9e3b477b2f5cd0a0999c233df6
devd4 <devendra.singh@d4insight.com>
2019-12-28 19:08:52 +0530

    first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@EnableAutoConfiguration
+@SpringBootApplication
+public class DemoApplication {
+	
+	@RequestMapping("/")
+	String hello() {
+		
+		return "HI Devendra";
+	}
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
