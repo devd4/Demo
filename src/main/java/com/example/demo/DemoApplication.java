ef91db4a50ffe6741d352581261a2d1ebe502360
devd4 <devendra.singh@d4insight.com>
2020-01-03 14:16:40 +0530

    hmm

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,22 +3,23 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 
 @RestController
 @EnableAutoConfiguration
 @SpringBootApplication
-public class DemoApplication {
-	
+public class DemoApplication extends SpringBootServletInitializer{
+
 	@RequestMapping("/")
 	String hello() {
-		
+
 		return "Welcome To D4 Insight.";
 	}
-	
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
